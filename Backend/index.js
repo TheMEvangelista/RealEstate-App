f60e8315af94974ac7d0280a919a43887400c5d6
Matheus <vincejulls2517@gmail.com>
2025-06-26 23:55:25 -0300

    Chore: Config the initial route

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { userRouter } from "./routes/userRoute";
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(PORT, () => {
-   console.log(`Server is runnig on port ${PORT}`)
-})
+  console.log(`Server is runnig on port ${PORT}`);
+});
+
+app.use("/api/user", userRouter);
