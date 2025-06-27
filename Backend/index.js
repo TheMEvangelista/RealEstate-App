3167cb81793ce345ca8b8c6a76acf555e2bf23a0
Matheus <vincejulls2517@gmail.com>
2025-06-26 23:31:56 -0300

    Chore: modifications in .env index.js package.json files

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,12 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+   console.log(`Server is runnig on port ${PORT}`)
+})
