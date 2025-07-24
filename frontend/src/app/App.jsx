29a954c2f9d26256b5200404e27b3462c599da2c
Matheus <vincejulls2517@gmail.com>
2025-07-24 11:31:51 -0300

    ...

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/Navbar";
+
+function App() {
+  return (
+    <main className="w-full flex flex-col">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
